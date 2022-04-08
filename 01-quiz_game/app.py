cfcdc3b20874bb9a1e1e922cead519f244d08221
juliaokataleko <juliofeli78@gmail.com>
2022-04-08 11:00:45 +0000

    finish quiz game

--- 01-quiz_game/app.py
+++ 01-quiz_game/app.py
@@ -2,14 +2,48 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play Yes (y|Y), No (n|N)? ")
 
-if playing.upper() != "YES" and playing.upper() != "Y":
+if playing.lower() != "yes" and playing.lower() != "y":
     quit()
 
-print("Ok! let's Play!")
+print("Ok! let's Play! :)")
+score = 0
 
 answer = input("What does CPU stand for? ")
 
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+
+answer = input("What Does RAM stand for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+
+answer = input("What does PSU stand for? ")
+
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print(f"You got {score} questions correct!")
+
+percent = (score/4) * 100
+print(f"You got {percent}%")
